743920f27dec08626d0a6ef13a83e1312ec33692
mb <115391907+FireHeart3198@users.noreply.github.com>
2023-11-16 21:49:52 +1100

    Add function for random computer selection

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let random = Math.floor(Math.random()*4)-1;
+    if (random === 1) {
+        return computerSelection = "rock"; 
+    } else if (random === 2) {
+        return computerSelection = "paper";
+    } else if (random === 3) {
+        return computerSelection = "scissors";
+    }
+}
