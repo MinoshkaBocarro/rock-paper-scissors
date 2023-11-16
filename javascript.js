ba004ca6347174b6a488d865d2ae72b8da465421
mb <115391907+FireHeart3198@users.noreply.github.com>
2023-11-16 22:32:57 +1100

    Add function for one round of game

--- javascript.js
+++ javascript.js
@@ -8,3 +8,23 @@ function getComputerChoice() {
         return computerSelection = "scissors";
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionAdjusted = playerSelection.toLowerCase();
+    if (playerSelectionAdjusted === computerSelection) {
+        alert("It's a Draw! You're on the same wavelength!") /*if just returning (while testing) don't use return = ..., returns error as expects an expression */
+    } else if (playerSelectionAdjusted === "rock" && computerSelection === "paper") {
+        alert("You lose... that paper is stronger than your rock??")
+    } else if (playerSelectionAdjusted === "rock" && computerSelection === "scissors") {
+        alert("You win!! Those scissors got demolished by your rock!")
+    } else if (playerSelectionAdjusted === "scissors" && computerSelection === "rock") {
+        alert("You lose... yeah your scissors stook no chance against that rock")
+    } else if (playerSelectionAdjusted === "scissors" && computerSelection === "paper") {
+        alert("You win! Cut up that paper prettily with your scissors")
+    } else if (playerSelectionAdjusted === "paper" && computerSelection === "scissors") {
+        alert("You lose... ouch your paper got cut by those scissors")
+    } else if (playerSelectionAdjusted === "paper" && computerSelection === "rock") {
+        alert("You win! Don't question how your paper beats rock okay")
+    }
+}
